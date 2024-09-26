68d32fca6547c1ecf534bfe46dc304b1b9a01536
Roope Mantere <roope.mantere12@gmail.com>
2024-09-26 06:13:45 +0300

    create default hello world

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,10 +3,9 @@ from flask_jwt_extended import JWTManager
 from neo4j import GraphDatabase
 
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
-jwt = JWTManager(app)
 
 # Neo4j connection
-driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
+# driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
 
-from .routes import *
+from .routes import main
+app.register_blueprint(main)
