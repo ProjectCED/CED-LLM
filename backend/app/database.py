8b4dc8fdfc26361ef4a18d6159771e2a671a9db8
Jani Vanhala <jani.vanhala@tuni.fi>
2024-10-01 13:29:36 +0300

    Added db deletion querys for properties

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,13 +5,15 @@
 #
 # Global settings:
 # db_add_settings_node() , creates settings node
-# db_modify_settings_data(property_name, data) , modifies settings specific property with specific data
-# db_lookup_settings_data(property_name) , lookup settings specific property data
+# db_modify_settings_data(property_name, data) , modifies specific property with specific data
+# db_lookup_settings_data(property_name) , lookup specific property data
+# db_delete_settings_data(property_name) , delete specific property data (also delete property)
 #
 # Subject area settings:
 # db_add_subject_area_node(node_name) , creates subject area node of specific name
-# db_modify_subject_area_data(node_name, property_name, data) , modify subject area specific property with specific data
-# db_lookup_subject_area_data(node_name, property_name) , lookup subject area specific property data
+# db_modify_subject_area_data(node_name, property_name, data) , modify specific property with specific data
+# db_lookup_subject_area_data(node_name, property_name) , lookup specific property data
+# db_delete_subject_area_data(name_a, property_name) , delete specific property data (also delete property)
 
 from neo4j import GraphDatabase
 
@@ -65,6 +67,17 @@ def db_modify_settings_data(property_name, data_a):
     )
     return "Settings " + property_name + " modified"
 
+# Removes specific Settings property data (and property).
+def db_delete_settings_data(property_name):
+    # define query string within python, because driver doesn't allow property types being a variable
+    query_string = "MATCH (n:Settings {name: '" + settings_node_name + "'}) REMOVE n." + property_name
+
+    driver.execute_query(
+        query_string,
+        database_="neo4j",
+    )
+    return "Settings " + property_name + " deleted"
+
 
 # Return data of specific property from settings (Base)
 def db_lookup_settings_data(property_name):
@@ -113,3 +126,16 @@ def db_lookup_subject_area_data(name, property_name):
         database_="neo4j",
     )
     return next(iter(records)).data()[property_name]
+
+
+# Removes specific SubjectArea-node's property data (and property).
+def db_delete_subject_area_data(name_a, property_name):
+    # define query string within python, because driver doesn't allow property types being a variable
+    query_string = "MATCH (n:SubjectArea {name: $name_b}) REMOVE n." + property_name
+
+    driver.execute_query(
+        query_string,
+        name_b = name_a,
+        database_="neo4j",
+    )
+    return "Subject area's " + name_a + "'s " + property_name + " deleted"
