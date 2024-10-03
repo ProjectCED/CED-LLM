40277fdef47bdeeb6c6d2b774d015017b5b563de
Vertti Nuotio <vertti.nuotio@gmail.com>
2024-10-03 10:28:09 +0300

    Azure GPT request template now works

--- backend/app/api_test.py
+++ backend/app/api_test.py
@@ -1,6 +1,7 @@
 import aiohttp
 import os
 from dotenv import load_dotenv
+import asyncio
 
 GPT_URL = "https://ced-llm.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2023-03-15-preview"
 load_dotenv()
@@ -8,5 +9,9 @@ GPT_KEY = os.getenv("GPT_KEY")
 
 async def get_gpt_response(prompt: str):
     async with aiohttp.ClientSession() as session:
-        async with session.post(GPT_URL, json={"prompt": prompt}, headers={"api-key": GPT_KEY}) as response:
+        # Role can be "user", "system", "tool" or "assistant"
+        message = {"role": "user", "content": prompt}
+        async with session.post(GPT_URL, json={"messages": [message]}, headers={"api-key": GPT_KEY}) as response:
+            if (response.status != 200):
+                response.raise_for_status()
             return await response.json()
