68d1313f6c795d84c9e1094e3b0dc1471fa20fa7
Ilona Timonen <ilona.s.timonen@tuni.fi>
2024-10-16 20:46:52 +0300

    UI for showing results from the analysis

--- frontend/src/Projects.jsx
+++ frontend/src/Projects.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './Projects.css';
 
 const Projects = () => {
-  return <h1>Hello World from Projects!</h1>;
+  return (
+    <div className="projects-container">
+      <h1>Hello World from Projects!</h1>
+      <div className="results-box">
+        Results from analysis here
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
