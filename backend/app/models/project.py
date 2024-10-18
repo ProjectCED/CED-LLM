cb705c71c31c4b83250c31bff1f61e53468aea38
Lauri Laine <laurilaine8@gmail.com>
2024-10-18 18:50:09 +0300

    added analyzed files saving

--- backend/app/models/project.py
+++ backend/app/models/project.py
@@ -12,11 +12,10 @@ class Project:
         self.save_analyzed_files(id_value)
         
     def save_analyzed_files(self, project_id):
-        self.__project_id = project_id
-    for file in self.__analyzed_files:
-        file_id = self.__database.add_file_node(file)
-        self.__database.add_relationship(project_id, NodeProperties.Project.NAME, self.__name)
+        for file in self.__analyzed_files:
+            file_id = self.__database.add_dataset_node(file)
+            self.__database.connect_dataset_to_project(file_id, project_id)
         
-        # Create result_blueprint node and add LLM result to it
-        result_id = self.__database.add_result_blueprint_node(project_id, data_sets, blueprints, datamodels)
-        self.__database.set_result_blueprint_property(result_id, NodeProperties.ResultBlueprint.RESULT, 'LLM text response here')
+            # Create result_blueprint node and add LLM result to it
+            result_id = self.__database.add_result_blueprint_node(project_id, [file], [], [])
+            self.__database.set_result_blueprint_property(result_id, NodeProperties.ResultBlueprint.RESULT, 'LLM text response here')
