aa3caf90bbd26d23b327903827fbb0466407d791
Vertti Nuotio <vertti.nuotio@gmail.com>
2024-10-18 20:18:55 +0300

    modify Blueprint and Project classes to simplify logic

--- backend/app/models/project.py
+++ backend/app/models/project.py
@@ -1,5 +1,6 @@
 from app.database import Database, NodeProperties
-
+from result import Result
+"""
 class Project:
     def __init__(self, name, analyzed_files):
         self.__name = name
@@ -19,3 +20,46 @@ class Project:
             # Create result_blueprint node and add LLM result to it
             result_id = self.__database.add_result_blueprint_node(project_id, [file], [], [])
             self.__database.set_result_blueprint_property(result_id, NodeProperties.ResultBlueprint.RESULT, 'LLM text response here')
+            """
+
+class Project:
+    def __init__(self, name: str, results: list):
+        self.__project_id = None
+        self.__name = name
+        self.__results = results
+        self.__database = Database()
+
+    def save_project(self):
+        self.__project_id = self.__database.add_project_node()
+        self.__database.set_project_property(self.__project_id, NodeProperties.Project.NAME, self.__name)
+        for result in self.__results:
+            self.add_result(result)
+
+    def add_result(self, result: Result):
+        self.__database.connect_result_blueprint_to_project(result.__result_id, self.__project_id)
+
+
+
+
+
+"""
+bp1 = Blueprint("blueprint1", ["question1?", "question2?"])
+bp1.save_blueprint()
+
+...
+...
+...
+
+# get selected blueprint? (first connect id to UI element and later get bp1 by id?)
+result1 = Result("file1.pdf", bp1, "This file contains... (from LLM)")
+result1.save_result()
+
+...
+
+project1 = Project("Project 1", [ result1 ])
+project1.save_project()
+
+
+result2 = Result("file2.pdf", bp1, "This file contains... (from LLM)")
+project1.add_result(result2)
+"""
