3d4da62eb2cea981f13eeaedd8af97852dbb72d4
Vertti Nuotio <vertti.nuotio@gmail.com>
2024-10-18 22:45:42 +0300

    reformat text extraction into generic function

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,17 +1,25 @@
 import pymupdf
+import os.path
 
-def extract_text_from_pdf(pdf_path: str) -> str:
-    pdf_document = pymupdf.open(pdf_path)
+def extract_text_from_file(filepath: str) -> str:
+    if not os.path.isfile(filepath):
+        return None
+    
     text = ""
-    for page in pdf_document:
-        text += page.get_text()
-    pdf_document.close()
-    return text
 
+    # .pdf extraction
+    if filepath.endswith(".pdf"):
+        with pymupdf.open(filepath) as file:
+            for page in file:
+                text += page.get_text()
 
-def extract_first_page_from_pdf(pdf_path: str) -> str:
-    pdf_document = pymupdf.open(pdf_path)
-    first_page = pdf_document[0]
-    text = first_page.get_text()
-    pdf_document.close()
+    # .txt extraction
+    elif filepath.endswith(".txt"):
+        with open(filepath, "r") as file:
+            text = file.read()
+
+    # Empty check to return None if no text was extracted
+    text = text.strip()
+    if text == "":
+        return None
     return text
