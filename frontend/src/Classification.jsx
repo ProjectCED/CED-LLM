89931ff72c0615f7a135eecaa62d81a7680b076b
Ella <ella.juvonen@tuni.fi>
2024-10-22 13:39:43 +0300

    Modifying the main container for classification page

--- frontend/src/Classification.jsx
+++ frontend/src/Classification.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
-import { Outlet, useNavigate} from 'react-router-dom';
-import { useEffect } from 'react';
+import MultiStepForm from './MultiStepForm';
 
 const Classification = () => {
 
-  const navigate = useNavigate();
-
-  // useEffect hook to navigate to the 'file-download' page on component mount
-  useEffect(() => {
-    navigate('/app/classification/file-download');
-  }, []); // Empty dependency array ensures this runs only once when the component mounts
-
   return (
-    <div className="classification-page"> {/* Main container for the classification page */}
-      <Outlet />
+    <div className="Classification"> {/* Main container for the classification page */}
+      <MultiStepForm />
     </div>
   );
 };
