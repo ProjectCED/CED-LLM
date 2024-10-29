7f5ab433b8e3ade75951606aedc6b9937d1acab7
Vertti Nuotio <vertti.nuotio@gmail.com>
2024-10-29 13:56:56 +0200

    implement uploading and reading file, currently saves file

--- backend/app/backend_api.py
+++ backend/app/backend_api.py
@@ -1,15 +1,30 @@
-from flask import Flask, jsonify, Blueprint
+from flask import Flask, jsonify, Blueprint, request
 from flask_cors import CORS
-from app.api_handler import analyze_file
+from app.api_handler import ApiHandler
+import app.utils as utils
 
 main = Blueprint('main', __name__)
+apiHandler = ApiHandler()
 
 CORS(main)  # Allows connections between domains
 
 @main.route('/analyze', methods=['POST'])
 def analyze():
-    result = analyze_file()
-    return jsonify(result=result)
+    file = request.files['file']
+    file.save(file.filename)
+
+    results = apiHandler.test_file_read(file.filename)
+
+    return jsonify(results)
+
+@main.route('/test_analyze', methods=['POST'])
+def test_analyze():
+    file = request.files['file']
+    file.save(file.filename)
+
+    results = apiHandler.test_file_read(file.filename)
+
+    return jsonify(results)
 
 if __name__ == '__main__':
     main.run(debug=True)
