fed3ea74e7d460d6618872a10bc22744a8b53cfa
Jani Vanhala <jani.vanhala@tuni.fi>
2024-10-29 13:31:34 +0200

    Restructure and added projects and result_blueprint

--- backend/app/database_dummy.py
+++ backend/app/database_dummy.py
@@ -3,34 +3,18 @@ from database import Database, NodeProperties
 class DatabaseDummy:
     '''Create pre-filled dummy database'''
     def __init__(self):
-        '''Initialize'''
+        '''When editing, be sure to have all names unique for this example to work'''
         self.db = Database()
 
-        # clear all
+        ### clear all
         self.__clear()
 
-        # global settings
+        ### global settings
         self.__global()
 
-        # user settings
-        self.__users()
-
-        # blueprints
-        self.__blueprints()
-
-
-    def __clear(self):
-        '''Clear database'''
-        self.db.debug_clear_all()
-
-    def __global(self):
-        '''Global settings'''
-        self.db.add_global_settings_node()
-
-    def __users(self):
-        '''User Settings'''
+        ### user settings
         # [(name, email)]
-        users = [
+        __users = [
             (
                 'John Smith',
                 'john.smith@example.com',
@@ -48,17 +32,13 @@ class DatabaseDummy:
                 'michael.brown@example.com',
                 ),
         ]
-        for user in users:
-            # Create nodes with email
-            id = self.db.add_user_settings_node(user[1])
-            self.db.set_user_settings_property(id, NodeProperties.UserSettings.NAME, user[0])
+        self.__users(__users)
 
-    def __blueprints(self):
-        '''Blueprints'''
+        ### blueprints
         # [(name, description)]
-        blueprints = [
+        __blueprints = [
             (
-                'Eduskunta',
+                'Eduskunta ',
                 'A collaborative framework designed to enhance educational dialogue and decision-making through structured discussions and feedback.',
                 ),
             (
@@ -86,10 +66,141 @@ class DatabaseDummy:
                 'A playful exploration that drives discovery and insight through a series of engaging and thought-provoking questions.',
                 ),
         ]
+        self.__blueprints(__blueprints)
 
+        ### projects
+        # [(name)]
+        __projects = [
+            (
+                'Eduskunta',
+                ),
+            (
+                'Performance Review',
+                ),
+            (
+                'Operations Enhancement',
+                ),
+            (
+                'Quality Improvement',
+                ),
+            (
+                'Compliance Assessment',
+                ),
+        ]
+        self.__projects(__projects)
+
+        ### Result-Blueprint
+        # references __project names
+        # references __blueprint names
+        # [(project-name, blueprint-name, datetime.iso(), result, filename)]
+        __result_blueprint = [
+            (
+                __projects[1][0],
+                __blueprints[1][0],
+                '2023-05-15T09:45:15',
+                'Lorem Ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'employee_performance_review_2024.pdf',
+                ),
+            (
+                __projects[1][0],
+                __blueprints[2][0],
+                '2022-01-01T00:00:00',
+                'Lorem Ipsum nulla facilisi, sed dapibus leo a quam ullamcorper, eu hendrerit odio condimentum.',
+                'performance_evaluation_summary_Q1_2023.txt',
+                ),
+            (
+                __projects[1][0],
+                __blueprints[3][0],
+                '2021-12-31T23:59:59',
+                'Lorem Ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'annual_performance_feedback_2023.cvs',
+                ),
+            (
+                __projects[2][0],
+                __blueprints[4][0],
+                '2021-09-15T19:00:00',
+                'Lorem Ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'efficiency_metrics_analysis_2023.txt',
+                ),
+            (
+                __projects[3][0],
+                __blueprints[5][0],
+                '2022-03-10T11:30:45',
+                'Lorem Ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'customer_feedback_analysis_2024.txt',
+                ),
+            (
+                __projects[4][0],
+                __blueprints[2][0],
+                '2023-06-20T16:45:00',
+                'Lorem Ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'efficiency_metrics_analysis_2023.txt',
+                ),
+            (
+                __projects[4][0],
+                __blueprints[4][0],
+                '2024-11-01T08:15:30',
+                'Lorem Ipsum sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                'compliance_assessment_report_2024.txt',
+                ),
+        ]
+        __result_blueprint(__result_blueprint)
+
+
+
+    def __clear(self):
+        '''Clear database'''
+        self.db.debug_clear_all()
+
+    def __global(self):
+        '''Global settings'''
+        self.db.add_global_settings_node()
+
+    def __users(self, users):
+        '''User Settings'''
+        for user in users:
+            # Create nodes with email
+            id = self.db.add_user_settings_node(user[1])
+            if not user[0] == None:
+                self.db.set_user_settings_property(id, NodeProperties.UserSettings.NAME, user[0])
+
+    def __blueprints(self, blueprints):
+        '''Blueprints'''
         for blueprint in blueprints:
             id = self.db.add_blueprint_node()
-            self.db.set_blueprint_property(id, NodeProperties.Blueprint.NAME, blueprint[0])
-            self.db.set_blueprint_property(id, NodeProperties.Blueprint.DESCRIPTION, blueprint[1])
+            if not blueprint[0] == None:
+                self.db.set_blueprint_property(id, NodeProperties.Blueprint.NAME, blueprint[0])
+            if not blueprint[1] == None:
+                self.db.set_blueprint_property(id, NodeProperties.Blueprint.DESCRIPTION, blueprint[1])
+
+
+    def __projects(self, projects):
+        '''Projects'''
+        for project in projects:
+            id = self.db.add_project_node()
+            if not project[0] == None:
+                self.db.set_project_property(id, NodeProperties.Project.NAME, project[0])
+
+
+    def __result_blueprint(self, results):
+        '''Result-Blueprint'''
+        nodes = self.db.lookup_project_nodes()
+        for result in results:
+            id = self.db.add_result_blueprint_node()
+            # connect results to specific projects
+            for project_id, project_name in nodes:
+                if project_name == result[0]:
+                    self.db.connect_result_blueprint_to_project(id, project_id)
+                    # properties
+                    if not result[2] == None:
+                        self.db.set_result_blueprint_property(id, NodeProperties.ResultBlueprint.DATETIME, result[2])
+                    if not result[3] == None:
+                        self.db.set_result_blueprint_property(id, NodeProperties.ResultBlueprint.RESULT, result[3])
+                    if not result[4] == None:
+                        self.db.set_result_blueprint_property(id, NodeProperties.ResultBlueprint.FILENAME, result[4])
+
+
+                    
+            
 
 
