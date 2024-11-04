47b06eca800ea33143ec2df811041413346d816c
Lauri-Niemela <150140860+Lauri-Niemela@users.noreply.github.com>
2024-11-04 08:27:02 +0200

    fix import & add test function

--- backend/app/backend_api.py
+++ backend/app/backend_api.py
@@ -1,14 +1,15 @@
 from flask import Flask, jsonify, Blueprint
 from flask_cors import CORS
-from app.api_handler import analyze_file
+from app.api_handler import ApiHandler
 
 main = Blueprint('main', __name__)
+apiHandler = ApiHandler()
 
 CORS(main)  # Allows connections between domains
 
-@main.route('/analyze', methods=['POST'])
+@main.route('/analyze', methods=['POST']) 
 def analyze():
-    result = analyze_file()
+    result = apiHandler.test_file_read()
     return jsonify(result=result)
 
 if __name__ == '__main__':
