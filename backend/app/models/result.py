a33c4b4b66d59697b933c93d5b8203c6aabb8596
Lauri Laine <laurilaine8@gmail.com>
2024-11-15 16:17:47 +0200

    added used blueprint copying and modified resultpy

--- backend/app/models/result.py
+++ backend/app/models/result.py
@@ -3,10 +3,10 @@ from app.database import NodeProperties
 from blueprint import Blueprint
 
 class Result:
-    def __init__(self, filename: str, used_blueprint_id, result):
+    def __init__(self, filename: str, blueprint_id, result):
         self.__result_id = None
         self.__filename = filename
-        self.__used_blueprint_id = used_blueprint_id
+        self.__blueprint_id = blueprint_id
         self.__result = result
         self.__database = Database()
 
@@ -14,4 +14,5 @@ class Result:
         self.__result_id = self.__database.add_result_blueprint_node()
         self.__database.set_result_blueprint_property(self.__result_id, NodeProperties.ResultBlueprint.FILENAME, self.__filename)
         self.__database.set_result_blueprint_property(self.__result_id, NodeProperties.ResultBlueprint.RESULT, self.__result)
-        self.__database.connect_used_blueprint_to_result_blueprint(self.__used_blueprint_id, self.__result_id)
+        used_blueprint_id = self.__database.copy_to_used_blueprint_node(self.__blueprint_id)
+        self.__database.connect_used_blueprint_to_result_blueprint(self.__blueprint_id, self.__result_id)
