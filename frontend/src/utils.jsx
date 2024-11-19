b804c1335bd2375deb3a69298eaa74a9ac62635e
Vertti Nuotio <vertti.nuotio@gmail.com>
2024-11-19 13:50:48 +0200

    move analysis function to utils and comment some relevant code in Projects

--- frontend/src/utils.jsx
+++ frontend/src/utils.jsx
@@ -5,3 +5,14 @@ export const getBlueprints = async () => {
     const blueprints = await response.json();
     return blueprints;
   };
+
+export const analyzeUploadedFile = async (filename) => {
+const response = await fetch('http://127.0.0.1:5000/analyze_file', {
+    method: 'POST',
+    body: filename,
+    });
+
+    let data = await response.json();
+    data = data.replace(/\\n/g, '<br />');
+    return data;
+}
