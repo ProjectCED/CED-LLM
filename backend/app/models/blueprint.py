a0b2101fc0e157ed7cf32ae1d03d2a70c3ecca15
Vertti Nuotio <vertti.nuotio@gmail.com>
2024-12-05 13:16:56 +0200

    change __blueprint_id to __blueprintId to respect naming conventions

--- backend/app/models/blueprint.py
+++ backend/app/models/blueprint.py
@@ -2,15 +2,15 @@ from app.database import Database, NodeProperties
 
 class Blueprint:
     def __init__(self, name: str, description: str, questions: list):
-        self.__blueprint_id = None
+        self.__blueprintId = None
         self.__name = name
         self.__description = description
         self.__questions = questions
         self.__database = Database()
 
     def save_blueprint(self):
-        self.__blueprint_id = self.__database.add_blueprint_node()
-        self.__database.set_blueprint_property(self.__blueprint_id, NodeProperties.Blueprint.NAME, self.__name)
-        self.__database.set_blueprint_property(self.__blueprint_id, NodeProperties.Blueprint.DESCRIPTION, self.__description)
-        self.__database.set_blueprint_property(self.__blueprint_id, NodeProperties.Blueprint.QUESTIONS, self.__questions)
-        return self.__blueprint_id
+        self.__blueprintId = self.__database.add_blueprint_node()
+        self.__database.set_blueprint_property(self.__blueprintId, NodeProperties.Blueprint.NAME, self.__name)
+        self.__database.set_blueprint_property(self.__blueprintId, NodeProperties.Blueprint.DESCRIPTION, self.__description)
+        self.__database.set_blueprint_property(self.__blueprintId, NodeProperties.Blueprint.QUESTIONS, self.__questions)
+        return self.__blueprintId
