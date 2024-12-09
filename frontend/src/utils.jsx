8394c50243cdd40dff9cf4fb9a124d810d0d0352
Vertti Nuotio <vertti.nuotio@gmail.com>
2024-12-09 12:42:42 +0200

    add JSDoc comments to utils.jsx

--- frontend/src/utils.jsx
+++ frontend/src/utils.jsx
@@ -1,4 +1,9 @@
 // Files & analysis
+/**
+ * Uploads a file to the backend server for later analysis.
+ * @param {File} file PDF or text file to upload to server for analysis.
+ * @returns {Promise<string>} Filename of the uploaded file.
+ */
 export const uploadFile = async (file) => {
     const formData = new FormData();
     formData.append('file', file);
@@ -10,6 +15,12 @@ export const uploadFile = async (file) => {
     return filename;
 };
 
+/**
+ * Analyzes an already uploaded file using the provided blueprint.
+ * @param {string} filename Filename of the uploaded file to analyze.
+ * @param {Object} blueprint Blueprint to use for analysis, containing questions for LLM.
+ * @returns {Promise<string>} Analysis result text with line breaks replaced by <br />.
+ */
 export const analyzeUploadedFile = async (filename, blueprint) => {
 const response = await fetch('/api/analyze_file', {
         method: 'POST',
@@ -24,6 +35,12 @@ const response = await fetch('/api/analyze_file', {
     return data;
 };
 
+/**
+ * Analyzes raw text using the provided blueprint.
+ * @param {string} text Raw text to be analyzed using LLM.
+ * @param {Object} blueprint Blueprint to use for analysis, containing questions for LLM.
+ * @returns {Promise<string>} Analysis result text with line breaks replaced by <br />.
+ */
 export const analyzeText = async (text, blueprint) => {
     const response = await fetch('/api/analyze_text', {
         method: 'POST',
@@ -38,6 +55,10 @@ export const analyzeText = async (text, blueprint) => {
 }
 
 // Blueprints
+/**
+ * Fetches all user-saved blueprints from the backend server.
+ * @returns {Promise<Array>} Array of blueprints with all properties.
+ */
 export const getBlueprints = async () => {
     const response = await fetch('/api/get_blueprints', {
       method: 'GET'
@@ -46,6 +67,12 @@ export const getBlueprints = async () => {
     return blueprints;
 };
 
+/**
+ * Saves a given blueprint to backend database.
+ * @param {Object} blueprint Blueprint with all properties to be saved.
+ *  If the blueprint contains an ID, it will be updated in the database.
+ * @returns {Promise<string>} ID of the saved/updated blueprint.
+ */
 export const saveBlueprint = async (blueprint) => {
     const response = await fetch('/api/save_blueprint', {
         method: 'POST',
@@ -58,6 +85,11 @@ export const saveBlueprint = async (blueprint) => {
     return id;
 };
 
+/**
+ * Deletes the blueprint with given ID from the backend database.
+ * @param {string} id UUID-type ID of the blueprint to be deleted.
+ * @returns {Promise<Boolean>} True if the blueprint was successfully deleted, false if not.
+ */
 export const deleteBlueprint = async (id) => {
   const response = await fetch('/api/delete_blueprint', {
       method: 'POST',
@@ -71,6 +103,12 @@ export const deleteBlueprint = async (id) => {
 };
 
 // Projects
+/**
+ * Creates a new project with the given name, saving it to the backend database.
+ *  The project will be have an empty results list initially.
+ * @param {string} projectName Name of the new project. 
+ * @returns {Promise<string>} ID of the newly created project.
+ */
 export const saveProject = async (projectName) => {
     const response = await fetch('/api/save_project', {
         method: 'POST',
@@ -83,6 +121,12 @@ export const saveProject = async (projectName) => {
     return id;
 }
 
+/**
+ * Fetches all projects from the backend server. Projects contain
+ *  their ID, name, and results list. Results contain all relevant data,
+ *  including the blueprint used for analysis.
+ * @returns {Promise<Array>} Array of projects with all properties.
+ */
 export const getProjects = async () => {
     const response = await fetch('/api/get_projects', {
         method: 'GET'
@@ -91,6 +135,12 @@ export const getProjects = async () => {
     return projects;
 };
 
+/**
+ * Deletes a project from the backend database. Also deletes all results
+ *  under the project (but not their blueprints).
+ * @param {string} id ID of the project to be deleted. 
+ * @returns {Promise<Boolean>} True if the project was successfully deleted, false if not.
+ */
 export const deleteProject = async (id) => {
     const response = await fetch('/api/delete_project', {
         method: 'POST',
@@ -104,6 +154,11 @@ export const deleteProject = async (id) => {
 }
 
 // Results
+/**
+ * Saves an analysis result to the backend database.
+ * @param {Object} result Result object with all properties except ID attached.
+ * @returns {Promise<string>} ID of the saved result.
+ */
 export const saveResult = async (result) => {
     const response = await fetch('/api/save_result', {
         method: 'POST',
@@ -117,6 +172,11 @@ export const saveResult = async (result) => {
     return id;
 }
 
+/**
+ * Deletes an analysis result from the backend database. Will NOT delete attached blueprint.
+ * @param {string} id ID of the result to be deleted.
+ * @returns {Promise<Boolean>} True if the result was successfully deleted, false if not.
+ */
 export const deleteResult = async (id) => {
     const response = await fetch('/api/delete_result', {
         method: 'POST',
